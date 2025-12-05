97a1bc9b95fcd53699c99b4495944c75e8e6a97a
barakidg <barakidg@gmail.com>
2025-12-05 23:21:01 +0300

    fix: Switched Sequelize dialect to postgres and updated package-lock for PG driver

--- simple backend/config/db.config.js
+++ simple backend/config/db.config.js
@@ -3,7 +3,7 @@ import "dotenv/config"
 
 const db = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: 'postgres',
     pool: {
         max: 10,
         min: 0,
